ccd86e77e1a5b9dc0c04267f8ffb29565fc061f7
DESKTOP-U7QOK1Q\Puesto1 <csi22-irodhan@colegioaltair.net>
2023-11-09 11:49:43 +0100

    91123

--- src/main/java/Daos/Acceso.java
+++ src/main/java/Daos/Acceso.java
@@ -19,7 +19,7 @@ public class Acceso {
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	@Column(name="id_acceso", nullable=false)
 	private long id_acceso;
-	
+
 	@Column(name="codigo_acceso", nullable=false)
 	private String codigo_acceso;
 	
@@ -27,8 +27,43 @@ public class Acceso {
 	private String descripcion_acceso;
 	
     @OneToMany(mappedBy="acceso")
-    List<Usuario> usuariosConAcceso;     
+    List<Usuario> usuariosConAcceso;  
 
+    
+    //paco
+    //Getters & Setters
+    public long getId_acceso() {
+		return id_acceso;
+	}
+
+	public void setId_acceso(long id_acceso) {
+		this.id_acceso = id_acceso;
+	}
+
+	public String getCodigo_acceso() {
+		return codigo_acceso;
+	}
+
+	public void setCodigo_acceso(String codigo_acceso) {
+		this.codigo_acceso = codigo_acceso;
+	}
+
+	public String getDescripcion_acceso() {
+		return descripcion_acceso;
+	}
+
+	public void setDescripcion_acceso(String descripcion_acceso) {
+		this.descripcion_acceso = descripcion_acceso;
+	}
+
+	public List<Usuario> getUsuariosConAcceso() {
+		return usuariosConAcceso;
+	}
+
+	public void setUsuariosConAcceso(List<Usuario> usuariosConAcceso) {
+		this.usuariosConAcceso = usuariosConAcceso;
+	}
+    
 	//CONSTRUCTORES
 	public Acceso() {
 		super();
--- src/main/java/Servicios/Insertar.java
+++ src/main/java/Servicios/Insertar.java
@@ -1,5 +1,56 @@
 package Servicios;
 
-public class Insertar {
+import Daos.Acceso;
+import Daos.Colecciones;
+import Daos.Editoriales;
+import Daos.Estado_Prestamo;
+import Daos.Generos;
+import Daos.Libros;
+import Daos.Prestamos;
+import Daos.Usuario;
+import jakarta.persistence.EntityManager;
 
+public class Insertar {
+	public void InsertarUsuario(EntityManager em,Usuario usu){
+		em.getTransaction().begin();
+		em.persist(usu);
+		em.getTransaction().commit();
+		
+	}
+	public void InsertarAcceso(EntityManager em,Acceso acceso){
+		em.getTransaction().begin();
+		em.persist(acceso);
+		em.getTransaction().commit();
+	}
+	public void InsertarLibro(EntityManager em,Libros libro){
+		em.getTransaction().begin();
+		em.persist(libro);
+		em.getTransaction().commit();
+	}
+	
+	public void InsertarColeccion(EntityManager em,Colecciones coleccion){
+		em.getTransaction().begin();
+		em.persist(coleccion);
+		em.getTransaction().commit();
+	}
+	public void InsertarPrestamos(EntityManager em,Prestamos prestamo){
+		em.getTransaction().begin();
+		em.persist(prestamo);
+		em.getTransaction().commit();
+	}
+	public void InsertarEditorial(EntityManager em,Editoriales editorial){
+		em.getTransaction().begin();
+		em.persist(editorial);
+		em.getTransaction().commit();
+	}
+	public void InsertarGenero(EntityManager em,Generos genero){
+		em.getTransaction().begin();
+		em.persist(genero);
+		em.getTransaction().commit();
+	}
+	public void InsertarEstadoPrestamo(EntityManager em,Estado_Prestamo estapres){
+		em.getTransaction().begin();
+		em.persist(estapres);
+		em.getTransaction().commit();
+	}
 }
